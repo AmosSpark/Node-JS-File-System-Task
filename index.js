fc26f78d308623227fb30955d7ba1e87e40e15f1
Ayobami Oguntolu <Ayobamiolugboyega@gmail.com>
2021-04-19 14:09:26 +0100

    feat(dir/path): ADD result/posts.json
    + Create a result directory and add file posts.json to the dir.

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const fetch = require("node-fetch");
 
+const fs = require("fs");
+const path = require("path");
 const url = "http://jsonplaceholder.typicode.com/posts";
 
+// CREATE result/posts.json
+// Create 'result' dir.
+fs.mkdir(path.join(__dirname, "/result"), {}, err => {
+  if (err) {
+    throw Error
+  } else {
+      console.log(`> Folder created 😊\n> View folder here: ${path.join(__dirname, "result")}`);
+      // Create 'posts.json' file in 'result' dir.
+      fs.writeFile(path.join(__dirname, "/result", "/posts.json"), "", err => {
+        if (err) {
+          throw Error
+        } else {
+            console.log(`> File created 😊\n> View file here: ${path.join(__dirname, "result", "posts.json")}`);
+        }
+      });
+  }
+});
+
+// Get all post
 fetch(url)
   .then(res => res.json())
   .then(data => console.log(`😊 Posts successfuly fetched.\n`,data))
